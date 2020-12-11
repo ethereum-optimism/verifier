6c1fb8de06e47d0bcf0021c9394bcd49c3f47710
Mark Tyneway <mark.tyneway@gmail.com>
2020-12-10 19:14:43 -0800

    progress: more

--- createKey.js
+++ createKey.js
@@ -1,23 +1,33 @@
 const {Wallet} = require('@ethersproject/wallet')
 const fs = require('fs/promises')
+const path = require('path')
 
 const KEY = 'L1_WALLET_KEY'
+const cfg = path.join(__dirname, 'docker-compose.env')
 
 ;(async () => {
   const wallet = Wallet.createRandom()
   const address = await wallet.getAddress()
   const key = wallet.privateKey
 
-  const file = await fs.readFile('docker-compose.env', 'utf-8')
+  const file = await fs.readFile(cfg, 'utf-8')
   const lines = file.split('\n')
 
   const out = []
   for (let line of lines) {
-    if (line === `#${KEY}=`)
+    if (line.startsWith(KEY)) {
+      const [,key] = line.split('=')
+      const w = new Wallet(key)
+      const addr = await w.getAddress()
+      console.log(`Fund the address ${addr}`)
+      process.exit(0)
+    }
+    if (line.startsWith(`#${KEY}=`))
       line = `${KEY}=${key}`;
     out.push(line)
   }
-  await fs.writeFile('docker-compose.env', out.join('\n'), 'utf-8')
+  await fs.writeFile(cfg, out.join('\n'), 'utf-8')
+  console.log(`Fund the address ${address}`)
 })().catch(err => {
   console.log(err)
   process.exit(1)
