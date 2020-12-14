2536beee538b435d5e56aca18ffc002f1a1d4e6a
Mark Tyneway <mark.tyneway@gmail.com>
2020-12-14 12:27:10 -0800

    setup: more polish

--- createKey.js
+++ createKey.js
@@ -19,7 +19,7 @@ const cfg = path.join(__dirname, 'docker-compose.env')
       const [,key] = line.split('=')
       const w = new Wallet(key)
       const addr = await w.getAddress()
-      console.log(`Fund the address ${addr}`)
+      log(addr)
       process.exit(0)
     }
     if (line.startsWith(`#${KEY}=`))
@@ -27,8 +27,13 @@ const cfg = path.join(__dirname, 'docker-compose.env')
     out.push(line)
   }
   await fs.writeFile(cfg, out.join('\n'), 'utf-8')
-  console.log(`Fund the address ${address}`)
+  log(address)
 })().catch(err => {
   console.log(err)
   process.exit(1)
 })
+
+function log(address) {
+  console.log(`Generated address: ${address}`)
+  console.log('Do not send mainnet funds to this address, key is stored in plaintext.')
+}
